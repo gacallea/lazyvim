203da5434010bcc2e404c131490a44539b144324
gacallea <gacallea@users.noreply.github.com>
2023-12-18 18:52:38 +0100

    js

---
+++ lua/plugins/extras/lang/javascript.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then vim.list_extend(opts.ensure_installed, { "javascript" }) end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    ft = { "elm" },
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "standardjs" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "standardjs" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        javascript = { "standardjs" },
+      },
+    },
+  },
+}
