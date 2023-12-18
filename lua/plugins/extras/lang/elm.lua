51bf3942ee2388e49c57dc7e9e55a06ed2736e34
gacallea <gacallea@users.noreply.github.com>
2023-12-18 15:05:13 +0100

    typo fix and ft

--- lua/plugins/extras/lang/elm.lua
+++ lua/plugins/extras/lang/elm.lua
@@ -7,6 +7,7 @@ return {
   },
   {
     "williamboman/mason.nvim",
+    ft = { "elm" },
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "elm-language-server", "elm-format" })
@@ -22,9 +23,10 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    ft = { "elm" },
     opts = {
       servers = {
-        elm_ls = {},
+        elmls = {},
       },
     },
   },
