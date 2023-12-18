10796f1b6d120585cd30e0da30de268ad885213b
gacallea <gacallea@users.noreply.github.com>
2023-12-18 22:56:50 +0100

    keeping mode for now

--- lua/plugins/extras/ui/lualine.lua
+++ lua/plugins/extras/ui/lualine.lua
@@ -44,7 +44,6 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, lsp)
-      opts.sections.lualine_a = {}
       opts.sections.lualine_z = { clock }
     end,
   },
