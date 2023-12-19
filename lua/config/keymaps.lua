7a8a21ad3e50ef9b686b7342a36e86f6d721b13c
gacallea <gacallea@users.noreply.github.com>
2023-12-19 20:41:31 +0100

    keys for buffer prev next

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.del({ "n" }, "<S-h>")
+vim.keymap.del({ "n" }, "<S-l>")
+vim.keymap.set("n", "<M-Left>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+vim.keymap.set("n", "<M-Right>", "<cmd>bnext<cr>", { desc = "Next buffer" })
