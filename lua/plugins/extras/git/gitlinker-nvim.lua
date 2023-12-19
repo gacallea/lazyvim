e609c2e5057d1cf66a8b517849d77504e3d2fd51
gacallea <gacallea@users.noreply.github.com>
2023-12-19 10:35:30 +0100

    no dep for plenary

--- lua/plugins/extras/git/gitlinker-nvim.lua
+++ lua/plugins/extras/git/gitlinker-nvim.lua
@@ -2,7 +2,6 @@ return {
   {
     "linrongbin16/gitlinker.nvim",
     event = "BufRead",
-    dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
       { "<leader>gy", "<cmd>GitLink<CR>", desc = "Git link copy", mode = { "n", "v" } },
       { "<leader>gz", "<cmd>GitLink!<CR>", desc = "Git link open", mode = { "n", "v" } },
