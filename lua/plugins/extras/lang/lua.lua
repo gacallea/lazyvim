64bd7938845636b96bd028b0275e089d58305933
gacallea <gacallea@users.noreply.github.com>
2023-12-20 11:35:25 +0100

    luacheck to selene

--- lua/plugins/extras/lang/lua.lua
+++ lua/plugins/extras/lang/lua.lua
@@ -11,7 +11,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "stylua", "luacheck" })
+      vim.list_extend(opts.ensure_installed, { "stylua", "selene" })
     end,
   },
   {
@@ -26,7 +26,7 @@ return {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
-        lua = { "luacheck" },
+        lua = { "selene" },
       },
     },
   },
