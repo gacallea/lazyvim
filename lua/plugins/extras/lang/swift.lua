61933d07d1aceef1d924a5cb98078b38cc019874
gacallea <gacallea@users.noreply.github.com>
2023-12-21 11:16:33 +0100

    not working yt

--- lua/plugins/extras/lang/swift.lua
+++ lua/plugins/extras/lang/swift.lua
@@ -1,3 +1,4 @@
+local swift_ft = { "swift", "objc", "objcpp" }
 local util = require "lspconfig.util"
 local pattern = "[^:]+:(%d+):(%d+): (%w+): (.+)"
 local groups = { "lnum", "col", "severity", "message" }
@@ -13,25 +14,6 @@ return {
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then vim.list_extend(opts.ensure_installed, { "swift" }) end
     end,
-  },
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = function(_, opts)
-  --     opts.ensure_installed = opts.ensure_installed or {}
-  --     vim.list_extend(opts.ensure_installed, { "sourcekit-lsp" })
-  --   end,
-  -- },
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = function(_, opts)
-          opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "codelldb" })
-        end,
-      },
-    },
   },
   {
     "stevearc/conform.nvim",
@@ -65,8 +47,16 @@ return {
       },
     },
   },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = opts.ensure_installed or {}
+  --     vim.list_extend(opts.ensure_installed, { "sourcekit-lsp" })
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
+    ft = swift_ft,
     opts = {
       servers = {
         sourcekit = {
@@ -97,6 +87,7 @@ return {
   },
   {
     "wojciech-kulik/xcodebuild.nvim",
+    ft = swift_ft,
     dependencies = {
       "nvim-telescope/telescope.nvim",
       "MunifTanjim/nui.nvim",
@@ -112,22 +103,100 @@ return {
       { "<leader>Xp", "<cmd>XcodebuildSelectTestPlan<cr>", desc = "Select Test Plan", mode = "n" },
       { "<leader>Xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", desc = "Toggle Code Coverage", mode = "n" },
       { "<leader>XC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", desc = "Show Code Coverage Report", mode = "n" },
-      { "<leader>Xq", "<cmd>Telescope quickfix<cr>", desc = "Show QuickFix List", mode = "n" },
     },
     config = function()
       require("xcodebuild").setup {
+        restore_on_start = true, -- logs, diagnostics, and marks will be loaded on VimEnter (may affect performance)
+        auto_save = true, -- save all buffers before running build or tests (command: silent wa!)
+        show_build_progress_bar = true, -- shows [ ...    ] progress bar during build, based on the last duration
+        prepare_snapshot_test_previews = true, -- prepares a list with failing snapshot tests
+        test_search = {
+          file_matching = "filename_lsp", -- one of: filename, lsp, lsp_filename, filename_lsp. Check out README for details
+          target_matching = true, -- checks if the test file target matches the one from logs. Try disabling it in case of not showing test results
+          lsp_client = "sourcekit", -- name of your LSP for Swift files
+          lsp_timeout = 200, -- LSP timeout in milliseconds
+        },
         logs = {
-          auto_open_on_success_tests = false,
-          auto_open_on_failed_tests = true,
-          auto_open_on_success_build = false,
-          auto_open_on_failed_build = true,
-          auto_focus = false,
-          auto_close_on_app_launch = true,
+          auto_open_on_success_tests = false, -- open logs when tests succeeded
+          auto_open_on_failed_tests = false, -- open logs when tests failed
+          auto_open_on_success_build = false, -- open logs when build succeeded
+          auto_open_on_failed_build = false, -- open logs when build failed
+          auto_close_on_app_launch = false, -- close logs when app is launched
+          auto_close_on_success_build = false, -- close logs when build succeeded (only if auto_open_on_success_build=false)
+          auto_focus = true, -- focus logs buffer when opened
+          filetype = "objc", -- file type set for buffer with logs
+          open_command = "silent bo split {path} | resize 20", -- command used to open logs panel. You must use {path} variable to load the log file
+          logs_formatter = "xcbeautify --disable-colored-output", -- command used to format logs, you can use "" to skip formatting
+          only_summary = false, -- if true logs won't be displayed, just xcodebuild.nvim summary
+          show_warnings = true, -- show warnings in logs summary
+        },
+        quickfix = {
+          show_errors_on_quickfixlist = true, -- add build/test errors to quickfix list
+          show_warnings_on_quickfixlist = true, -- add build warnings to quickfix list
         },
         code_coverage = {
-          enabled = false,
+          enabled = false, -- generate code coverage report and show marks
+          file_pattern = "*.swift", -- coverage will be shown in files matching this pattern
         },
       }
     end,
   },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   ft = swift_ft,
+  --   dependencies = {
+  --     {
+  --       "williamboman/mason.nvim",
+  --       opts = function(_, opts)
+  --         opts.ensure_installed = opts.ensure_installed or {}
+  --         vim.list_extend(opts.ensure_installed, { "codelldb" })
+  --       end,
+  --     },
+  --   },
+  --   keys = {
+  --     { "<leader>XD", "<cmd>xcodebuild.build_and_debug<cr>", desc = "Build & Debug", mode = "n" },
+  --     { "<leader>XR", "<cmd>xcodebuild.debug_without_build<cr>", desc = "Debug Without Building", mode = "n" },
+  --   },
+  --   config = function()
+  --     local dap = require "dap"
+  --     local xcodebuild = require "xcodebuild.dap"
+
+  --     dap.configurations.swift = {
+  --       {
+  --         name = "iOS App Debugger",
+  --         type = "codelldb",
+  --         request = "attach",
+  --         program = xcodebuild.get_program_path,
+  --         -- alternatively, you can wait for the process manually
+  --         -- pid = xcodebuild.wait_for_pid,
+  --         cwd = "${workspaceFolder}",
+  --         stopOnEntry = false,
+  --         waitFor = true,
+  --       },
+  --     }
+
+  --     dap.adapters.codelldb = {
+  --       type = "server",
+  --       port = "13000",
+  --       executable = {
+  --         -- set path to the downloaded codelldb
+  --         -- sample path: "/Users/YOU/Downloads/codelldb-aarch64-darwin/extension/adapter/codelldb"
+  --         command = "/path/to/codelldb/extension/adapter/codelldb",
+  --         args = {
+  --           "--port",
+  --           "13000",
+  --           "--liblldb",
+  --           -- make sure that this path is correct on your side
+  --           "/Applications/Xcode.app/Contents/SharedFrameworks/LLDB.framework/Versions/A/LLDB",
+  --         },
+  --       },
+  --     }
+
+  --     -- disables annoying warning that requires hitting enter
+  --     local orig_notify = require("dap.utils").notify
+  --     require("dap.utils").notify = function(msg, log_level)
+  --       if not string.find(msg, "Either the adapter is slow") then orig_notify(msg, log_level) end
+  --     end
+  --   end,
+  -- },
 }
