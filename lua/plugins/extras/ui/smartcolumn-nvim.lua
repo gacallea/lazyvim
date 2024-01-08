bc6f6979b65b4f30f11f1412025e32beaa3012d3
gacallea <gacallea@users.noreply.github.com>
2024-01-08 14:25:20 +0100

    smartcolumn

---
+++ lua/plugins/extras/ui/smartcolumn-nvim.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = { "80", "120" },
+      disabled_filetypes = {
+        "dashboard",
+        "neo-tree",
+        "lazy",
+        "mason",
+        "help",
+        "checkhealth",
+        "lspinfo",
+        "noice",
+        "Trouble",
+        "fish",
+        "zsh",
+      },
+    },
+  },
+}
