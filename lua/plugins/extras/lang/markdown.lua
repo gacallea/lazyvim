8bb56d4b100fc5390cf96600146776e7429099fb
gacallea <gacallea@users.noreply.github.com>
2024-01-08 14:58:45 +0100

    md preview to glow

--- lua/plugins/extras/lang/markdown.lua
+++ lua/plugins/extras/lang/markdown.lua
@@ -40,17 +40,22 @@ return {
     },
   },
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    keys = {
-      {
-        "<leader>cp",
-        ft = "markdown",
-        "<cmd>MarkdownPreviewToggle<cr>",
-        desc = "Markdown Preview",
-      },
-    },
-    config = function() vim.cmd [[do FileType]] end,
+    "ellisonleao/glow.nvim",
+    cmd = "Glow",
+    opts = {},
   },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   build = function() vim.fn["mkdp#util#install"]() end,
+  --   keys = {
+  --     {
+  --       "<leader>cp",
+  --       ft = "markdown",
+  --       "<cmd>MarkdownPreviewToggle<cr>",
+  --       desc = "Markdown Preview",
+  --     },
+  --   },
+  --   config = function() vim.cmd [[do FileType]] end,
+  -- },
 }
