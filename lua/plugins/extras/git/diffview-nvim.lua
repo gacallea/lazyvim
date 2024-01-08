903ed1a8b485195fc45f1d09e641b53de1fcb227
gacallea <gacallea@users.noreply.github.com>
2024-01-08 16:53:14 +0100

    diffview under git key

--- lua/plugins/extras/git/diffview-nvim.lua
+++ lua/plugins/extras/git/diffview-nvim.lua
@@ -2,5 +2,8 @@ return {
   {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen" },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Diff view", mode = { "n", "v" } },
+    },
   },
 }
