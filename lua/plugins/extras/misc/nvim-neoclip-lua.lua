f75d73aeba43a6ae880b488cb816355acbc7e18a
gacallea <gacallea@users.noreply.github.com>
2024-01-08 14:58:53 +0100

    neoclip

---
+++ lua/plugins/extras/misc/nvim-neoclip-lua.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "AckslD/nvim-neoclip.lua",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function(_, opts)
+      require("neoclip").setup(opts)
+      require("telescope").load_extension "neoclip"
+    end,
+    keys = {
+      { "<leader>fy", "<cmd>Telescope neoclip<cr>", desc = "Find yanks (neoclip)" },
+    },
+  },
+}
