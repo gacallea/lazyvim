e73c54241ec3b694ef02006d536568e8a7581d8b
gacallea <gacallea@users.noreply.github.com>
2024-01-10 16:09:32 +0100

    meh glow opts

--- lua/plugins/extras/lang/markdown.lua
+++ lua/plugins/extras/lang/markdown.lua
@@ -42,7 +42,10 @@ return {
   {
     "ellisonleao/glow.nvim",
     cmd = "Glow",
-    opts = {},
+    opts = {
+      width_ratio = 0.8, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
+      height_ratio = 0.8,
+    },
   },
   -- {
   --   "iamcco/markdown-preview.nvim",
