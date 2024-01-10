749c6b6e313130f22d491c77649fb0f74530e32e
gacallea <gacallea@users.noreply.github.com>
2024-01-10 16:09:41 +0100

    temp save

--- lua/plugins/extras/lang/chuck.lua
+++ lua/plugins/extras/lang/chuck.lua
@@ -1,3 +1,39 @@
 return {
-  { "NicholasDunham/chuck.nvim" },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>C"] = { name = "+chuck" },
+      },
+    },
+  },
+  {
+    -- "gacallea/chuck-nvim",
+    dir = "/Users/andreacfromtheapp/ProjectsandIdeas/Programming/Neovim/chuck-nvim",
+    name = "chuck-nvim",
+    dependencies = {
+      {
+        "nvim-tree/nvim-web-devicons",
+        opts = {
+          override_by_extension = {
+            ["ck"] = {
+              icon = "󰧚",
+              color = "#80ff00",
+              name = "ChucK",
+            },
+          },
+        },
+      },
+    },
+    ft = { "chuck" },
+    cmd = { "ChuckStatus", "ChuckAddShred", "ChuckRemoveShreds", "ChuckReplaceShred", "ChuckClearShreds" },
+    keys = {
+      { "<leader>Cs", "<cmd>ChuckStatus<cr>", desc = "Chuck Status", mode = "n" },
+      { "<leader>Ca", "<cmd>ChuckAddShred<cr>", desc = "Add Shred", mode = "n" },
+      { "<leader>Cd", "<cmd>ChuckRemoveShreds<cr>", desc = "Remove Shreds", mode = "n" },
+      { "<leader>Cr", "<cmd>ChuckReplaceShred<cr>", desc = "Replace Shred", mode = "n" },
+      { "<leader>Cc", "<cmd>ChuckClearShreds<cr>", desc = "Clear Shreds", mode = "n" },
+    },
+    config = function() require "chuck-nvim" end,
+  },
 }
