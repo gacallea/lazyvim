67f06af7556226fdd8e948fa08e40e9bb170c4bd
gacallea <gacallea@users.noreply.github.com>
2024-01-16 23:20:32 +0100

    proper minimap finally!

--- lua/plugins/extras/ui/codewindow-nvim.lua
+++ lua/plugins/extras/ui/codewindow-nvim.lua
@@ -4,9 +4,28 @@ return {
     keys = {
       { "<leader>um", function() require("codewindow").toggle_minimap() end, desc = "Toggle minimap" },
     },
-    config = function()
-      local codewindow = require "codewindow"
-      codewindow.setup()
-    end,
+    init = function() require "codewindow" end,
+    opts = {
+      auto_enable = true,
+      minimap_width = 18,
+      width_multiplier = 2,
+      screen_bounds = "background",
+      window_border = "none",
+      relative = "win",
+      exclude_filetypes = {
+        "dashboard",
+        "neo-tree",
+        "lazy",
+        "lazyterm",
+        "mason",
+        "help",
+        "checkhealth",
+        "lspinfo",
+        "noice",
+        "Trouble",
+        "fish",
+        "zsh",
+      },
+    },
   },
 }
