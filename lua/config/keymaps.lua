0e45d996b3357ea7402eb4cf2d719c5168ac4be0
gacallea <gacallea@users.noreply.github.com>
2024-02-10 11:52:06 +0100

    comment!

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,15 @@ vim.keymap.set("n", "<M-Left>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 vim.keymap.set("n", "<M-Right>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<F11>", "<cmd>TroubleToggle<cr>", { desc = "Trouble" })
 vim.keymap.set("n", "<F12>", "<cmd>AerialToggle<cr>", { desc = "Aerial (Symbols)" })
+vim.keymap.set(
+  "n",
+  "<leader>/",
+  function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end,
+  { desc = "Toggle comment for line" }
+)
+vim.keymap.set(
+  "v",
+  "<leader>/",
+  "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+  { desc = "Toggle comment for selection" }
+)
